f2c16ad2861973c81b65149b4abb2e5c278ef6a1
Ikechukwu Akujobi <iykejobi@gmail.com>
2022-02-13 22:55:41 -0500

    hide apikey and complete product detail route

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Setup Process
+# Setup Process
 
 1. npm init -y: Creates package.json
 2. npm install express request-promise nodemon: Install the express, request-promise and nodemon packages
@@ -6,7 +6,8 @@ Setup Process
 3. create index.js file: This is the api
 4. require express and request-promise: Express creates the backend application and request-promise allows us to make API requests
 5. const app = express(): Initialize the application with express as a function
-6. create the port (the one used is 6ß000)
+6. create the port (the one used is 6001)
+   NOTE: List of routes blocked - https://stackoverflow.com/questions/4313403/why-do-browsers-block-some-ports/22622633#22622633
 7. express.json(): Allow app to parse JSON input
 8. app.get(): This is just a message to let us know that the server is running
 9. app.listen(): Make the server listen on a specific port
@@ -15,3 +16,14 @@ Setup Process
    - Stop server: ctrl + c
    - Server crashed because request-promise needs a dependency. Need to install request
    - npm install request: Installs request. Server run smoothly!
+10. Sign up on scraperapi.com: It's a proxy API for web scraping
+    - Create an apiKey with it
+11. Create baseUrl: we can now make our own routes from our API to fetch specific products
+12. Create route to fetch product details - '/products/:productID'
+    - First get productID from parameters
+    - Get a response from scraperapi
+    - Send response back from the server
+    - Provide the error
+    - To run route on server, use http://localhost:6001/products/B08N5LFLC3
+    - B08N5LFLC3 is the productID and can get it from amazon url for a particular product.
+    - Example: a specific macbook air laptop url is https://www.amazon.com/Apple-MacBook-13-inch-512GB-Storage/dp/B08N5LFLC3/ref=sr_1_3?crid=1X2QLT72QF31O&keywords=macbook+air&qid=1644807769&sprefix=macbook+air%2Caps%2C64&sr=8-3
--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 // Import libraries
+require('dotenv').config() // allows us to use the .env library
 const express = require('express') // use express to create a backend application
 const request = require('request-promise') // use request-promise to make api requests
 
 // Initialize the application
 const app = express() // call express as a function
 // Create port
-const PORT = process.env.PORT || 6000
+const PORT = process.env.PORT || 6001
+
+// API key from scraperapi.com
+const apiKey = process.env.SCRAPER_API_KEY
+
+// Create base URL
+const baseUrl = `https://api.scraperapi.com?api_key=${apiKey}&autoparse=true` // we can now make our own routes from our API to fetch specific products and add extra parameters (use &)
 
 app.use(express.json()) // allows the application to parse JSON input
 
@@ -16,7 +23,28 @@ app.get('/', (req, res) => {
     res.send('Welcome to Amazon Scraper API!') // this is just a message to let us know that the server is running
 })
 
+// Create route to fetch product details
+// GET Product Details
+// :productID - the colon means that the product ID is going to be dynamic
+app.get('/products/:productID', async (req, res) => {
+    // First get productID from parameters
+    const { productID } = req.params // productID is going to be populated inside request.paramaters
+
+    try {
+        // Get a response from scraperapi
+        // Inside request, provide the first url and add extra parameters (use &)
+        // dp: this is product details
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/dp/${productID}`) // this is going to give us information for a specific product
+
+        // Send response back from the server
+        // res.json(response) // response in text format
+        res.json(JSON.parse(response)) // transforms result to JSON
+    } catch (err) {
+        res.json(err) // provide the error
+    }
+})
+
 // Last thing needed to start the server
 // Make the server listen on a specific port
 // ${PORT}: template strings which helps us to specify on which port we are currently running the application
-app.listen(PORT, () => console.log('Server listening on port ' + PORT))
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
