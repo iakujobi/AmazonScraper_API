b3bb566fce5775fea35987412f2701ba20581c61
Ikechukwu Akujobi <iykejobi@gmail.com>
2022-02-13 19:09:19 -0500

    import libraries

---
+++ index.js
@@ -0,0 +1,4 @@
+// Import libraries
+const express = require('express') // use express to create a backend application
+const request = require('request-promise') // use request-promise to make api requests
+
