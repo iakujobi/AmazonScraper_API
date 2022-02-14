32592469071b759bde9db805f381949406b4956b
Ikechukwu Akujobi <iykejobi@gmail.com>
2022-02-13 20:32:05 -0500

    end of barebone EXPRESS app

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ Setup Process
 3. create index.js file: This is the api
 4. require express and request-promise: Express creates the backend application and request-promise allows us to make API requests
 5. const app = express(): Initialize the application with express as a function
+6. create the port (the one used is 5000)
+7. express.json(): Allow app to parse JSON input
+8. app.get(): This is just a message to let us know that the server is running
+9. app.listen(): Make the server listen on a specific port
+   - At this point, this is the end bare bone EXPRESS application
--- index.js
+++ index.js
@@ -4,3 +4,19 @@ const request = require('request-promise') // use request-promise to make api re
 
 // Initialize the application
 const app = express() // call express as a function
+// Create port
+const PORT = process.env.PORT || 5000
+
+app.use(express.json()) // allows the application to parse JSON input
+
+// Every EXPRESS application needs at least one route
+// '/' is the initial route route
+// () is the callback function and inside it, we have the request and response and then return something
+app.get('/', (req, res) => {
+    res.send('Welcome to Amazon Scraper API!') // this is just a message to let us know that the server is running
+})
+
+// Last thing needed to start the server
+// Make the server listen on a specific port
+// ${PORT}: template strings which helps us to specify on which port we are currently running the application
+app.listen(PORT, () => console.log('Server running on port ${PORT}'))
